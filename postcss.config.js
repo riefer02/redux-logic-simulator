5fa98bfcb044c1fcf5743fe00d5623c4bb3d24bf
riefer02 <andrew.riefenstahl@gmail.com>
2021-02-11 20:11:56 -0600

    commit init

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.jsx",
+    // etc.
+  ],
+});
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
